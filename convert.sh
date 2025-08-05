1c9648bf5a8b06ed7173d71b56904e9dcb09ace3
Abhishek <38874099+abhisd123@users.noreply.github.com>
2025-08-05 15:36:23 +0530

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,4 +1,13 @@
+
 #!/bin/bash
+
+# Create output folders
 mkdir -p output
-asciidoctor -D output *.adoc
+mkdir -p docs
+
+# Convert all .adoc files to HTML and place in docs/ for GitHub Pages
+asciidoctor -D docs *.adoc
+
+# Convert all .adoc files to PDF and place in output/
 asciidoctor-pdf -D output *.adoc
+
